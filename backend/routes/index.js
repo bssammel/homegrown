6ed1b53c84302dcba37379bae9d980fd61b2bd0b
bssam <bssammel@gmail.com>
2023-09-19 21:13:50 -0400

    added error handling middleware for common errors and debugging and added an API route, this may be a duplicate commit, was in the wrong directory and files were not added to commits as expected

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);//prefixes all routes from the api folder with /api
 
 
 //Test Route
