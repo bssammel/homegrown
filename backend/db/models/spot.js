52edbcf88ef624190eb69e48565fc33a53d3bb4f
bssam <bssammel@gmail.com>
2023-09-29 10:44:18 -0400

    Added Spots table, as well as creating the associaton between spots and users

---
+++ backend/db/models/spot.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    
+    static associate(models) {
+      // define association here
+      Spot.belongsTo(
+        models.User,
+        {
+          foreignKey: 'ownerId',
+          onDelete:'CASCADE'
+        }
+      )
+
+    }
+  }
+  Spot.init({
+    ownerId: {type: DataTypes.INTEGER, allowNull:false},
+    address: {type: DataTypes.STRING, allowNull:false},
+    city: {type: DataTypes.STRING, allowNull:false},
+    state: {type: DataTypes.STRING, allowNull:false},
+    country: {type: DataTypes.STRING, allowNull:false},
+    lat: {type: DataTypes.DECIMAL, validate:{
+      max: 90,
+      min: -90,
+      isDecimal:true
+    }},
+    lng: {type: DataTypes.DECIMAL, validate:{
+      max: 180,
+      min: -180,
+      isDecimal:true
+    }},
+    name: {type: DataTypes.STRING, allowNull:false},
+    description: {type: DataTypes.STRING, allowNull:false},
+    price: {type: DataTypes.INTEGER, allowNull:false, validate: {min: 1} }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
