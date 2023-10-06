41d7d84ee490be4198d1a691b174560f2912d165
bssam <bssammel@gmail.com>
2023-10-06 14:27:55 -0400

    added functionality to get reviews by current user. will need to add previewImage column later.

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,5 +1,5 @@
 //first line
-const { User, Spot, SpotImage } = require('../../db/models');
+const { User, Spot, SpotImage, Review, ReviewImage } = require('../../db/models');
 const {requireAuth} = require('../../utils/auth.js')
 
 const express = require('express');
@@ -9,6 +9,33 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//! Get Reviews written by current user
+// TODO: need to make foreign key set up for preview image to pull in data from there and add the column to spot.previewImage
+router.get('/current', requireAuth, async (req,res) =>{
+    const {user} = req;
+    console.log("User Id?: ",user.id);
+    const userId = user.id;
+    const where = {};
+    where.userId = userId;
+    const currentUserReviews = await Review.findAll({
+        where,
+        include: [
+            {
+                model: User,
+                attributes: ['id','firstName','lastName']
+            },
+            {
+                model: Spot,
+                attributes: ['id','ownerId','address', 'city','state', 'country', 'lat', 'lng','name','price', ]
+            },
+            {
+                model: ReviewImage,
+                attributes: ['id','url']
+            },
+        ],
+    });
+    return res.json({currentUserReviews});
+})
 
 
 
