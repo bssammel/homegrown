205c7e1dc51cbcda55ad264a9f339d9657b4bb07
bssam <bssammel@gmail.com>
2023-10-06 12:42:47 -0400

    Created and fully tested edit a spot functionality

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,9 +9,48 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//! Validate spot creation array to reference below for data validation
+const validateSpotCreation = [
+    check('address')
+        .exists({ checkFalsy : true})
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy : true})
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy : true})
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy : true})
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy : true})
+        .isDecimal()
+        .isFloat({min: -90, max: 90})
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy : true})
+        .isDecimal()
+        .isFloat({min: -180, max: 180})
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy : true})
+        .isLength({min: 1, max: 49})
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy : true})
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy : true})
+        .isNumeric()
+        .withMessage('Price per day is requited'),
+    handleValidationErrors
+];
+
+
 
 // const router = require('express').Router();
-
+//! Get spots of current user
 router.get('/current', requireAuth, async (req,res) =>{
     const {user} = req;
     //console.log("User Id?: ",user.id);
@@ -65,6 +104,85 @@ router.post('/:spotId/images', requireAuth, async (req,res,next) =>{
         preview: newSpotImage.preview});
 })
 
+
+//! Edit a Spot 
+// TODO : Nothing, completed
+router.put('/:spotId', requireAuth, validateSpotCreation, async (req,res,next) =>{
+    // * isolate needed info
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    //isolate info for new spot data to update with
+    const spotId = req.params.spotId;//spot id to identify which spot to update
+    // console.log(`#####spotId:  ${spotId}`);
+    // * setting up new spot image
+    const spotToUpdate = await Spot.findByPk(req.params.spotId);//find spot that needs the new image
+
+    if(!spotToUpdate){//if we can't find the spot that needs the new image, then return err
+        const err = new Error('Spot couldn\'t be found');
+        err.status = 404;
+        return next(err);
+    }
+
+    // * Proper Auth! confirm if current user is owner of spot/Proper Auth!
+    const {user} = req;//
+    const userId = user.id;
+    const ownerId = spotToUpdate.ownerId;
+    if(userId !== ownerId){
+        const err = new Error('Forbidden');
+        err.title = 'Forbidden';
+        err.errors = { message: 'Forbidden' };
+        err.status = 403;
+        return next(err);
+    }
+
+    //* update spot
+    await spotToUpdate.update({
+        id: spotId,
+        ownerId: ownerId, 
+        address: address, 
+        city: city, 
+        state: state, 
+        country: country, 
+        lat: lat, 
+        lng: lng, 
+        name: name, 
+        description: description, 
+        price: price
+    })
+
+    // const updatedSpot = {
+    //     id: spotToUpdate.id,
+    //     ownerId: ownerId, 
+    //     address: spotToUpdate.address, 
+    //     city: spotToUpdate.city, 
+    //     state: spotToUpdate.state, 
+    //     country: spotToUpdate.country, 
+    //     lat: spotToUpdate.lat, 
+    //     lng: spotToUpdate.lng, 
+    //     name: spotToUpdate.name, 
+    //     description: spotToUpdate.description, 
+    //     price: spotToUpdate.price,
+    //     createdAt:spotToUpdate.createdAt,
+    //     updatedAt: spotToUpdate.updatedAt
+    // };
+   
+    return res.json({
+        id: spotToUpdate.id,
+            ownerId: ownerId, 
+            address: spotToUpdate.address, 
+            city: spotToUpdate.city, 
+            state: spotToUpdate.state, 
+            country: spotToUpdate.country, 
+            lat: spotToUpdate.lat, 
+            lng: spotToUpdate.lng, 
+            name: spotToUpdate.name, 
+            description: spotToUpdate.description, 
+            price: spotToUpdate.price,
+            createdAt:spotToUpdate.createdAt,
+            updatedAt: spotToUpdate.updatedAt
+    });
+})
+
+
 //!Get Spot based on spotId
 router.get('/:spotId', async (req,res,next) =>{
     const desiredSpot = await Spot.findByPk(req.params.spotId);
@@ -76,6 +194,7 @@ router.get('/:spotId', async (req,res,next) =>{
     return res.json(desiredSpot);
 })
 
+//! Get all spots
 router.get('/', async (req, res) =>{
     //where = {};
     const spots = await Spot.findAll();
@@ -85,41 +204,10 @@ router.get('/', async (req, res) =>{
 
 });
 
-const validateSpotCreation = [
-    check('address')
-        .exists({ checkFalsy : true})
-        .withMessage('Street address is required'),
-    check('city')
-        .exists({ checkFalsy : true})
-        .withMessage('City is required'),
-    check('state')
-        .exists({ checkFalsy : true})
-        .withMessage('State is required'),
-    check('country')
-        .exists({ checkFalsy : true})
-        .withMessage('Country is required'),
-    check('lat')
-        .exists({ checkFalsy : true})
-        .isDecimal()
-        .withMessage('Latitude is not valid'),
-    check('lng')
-        .exists({ checkFalsy : true})
-        .isDecimal()
-        .withMessage('Longitude is not valid'),
-    check('name')
-        .exists({ checkFalsy : true})
-        .isLength({min: 1, max: 49})
-        .withMessage('Name must be less than 50 characters'),
-    check('description')
-        .exists({ checkFalsy : true})
-        .withMessage('Desription is required'),
-    check('price')
-        .exists({ checkFalsy : true})
-        .isNumeric()
-        .withMessage('Price per day is requited'),
-    handleValidationErrors
-];
 
+
+
+//! add spot 
 router.post('/', requireAuth, validateSpotCreation, async (req,res, next) =>{ // TODO: not sure the require auth is functioning as expected here
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
     const {user} = req;
