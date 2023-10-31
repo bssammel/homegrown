f92d9575319c2483fe97cfbc1ef1fa59fde1127c
bssam <bssammel@gmail.com>
2023-10-31 17:12:13 -0400

    reformatted date reformatting function to work universally

--- backend/utils/date-time.js
+++ backend/utils/date-time.js
@@ -5,12 +5,15 @@ const reformatTimes = function (timestampArr, functionName){
     const newTimestamps = [];
     for (let i = 0; i < timestampArr.length; i++) {
         let timestamp = timestampArr[i];
-        // 2023-10-31 16:49:31.783 +00:00
         console.log(timestamp);
-        if(timestamp.length === 30){
-            timestamp = timestamp.slice(0,19);
-        }
-
+        // if(timestamp.length === 30 && functionName === "getAllSpots"){
+        //     timestamp = timestamp.slice(0,19);
+        // }
+        // if(timestamp.length === 24 && functionName === )
+        timestamp = timestamp.replace(/\D/g, '');
+        console.log(timestamp);
+        if(timestamp.length > 14) timestamp = timestamp.slice(0,14);
+        timestamp = timestamp.slice(0,4) + "-" + timestamp.slice(4,6) + "-" + timestamp.slice(6,8) + " " + timestamp.slice(8,10) + ":" + timestamp.slice(10,12) + ":"  + timestamp.slice(12,14);
 
         newTimestamps.push(timestamp);
     }
