295f4f0727993e4e0c695b8511f52d86ea73af58
bssam <bssammel@gmail.com>
2023-12-12 14:00:53 -0500

    making changes to front end package json for build script

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [
+//     react(),
+//     eslint({
+//       lintOnStart: true,
+//       failOnError: mode === "production"
+//     })
+//   ],
+//   // To automatically open the app in the browser whenever the server starts,
+//   // uncomment the following lines:
+//   // server: {
+//   //   open: true
+//   // }
+// }));
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -11,9 +25,9 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production"
     })
   ],
-  // To automatically open the app in the browser whenever the server starts,
-  // uncomment the following lines:
-  // server: {
-  //   open: true
-  // }
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    },
+  }
 }));
