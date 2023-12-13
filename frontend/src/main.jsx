fac38169d3fdaf6668ca8b3cb6ad2a403e6bce75
bssam <bssammel@gmail.com>
2023-12-13 17:57:42 -0500

    Added Log In Modal and restructured

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { restoreCSRF, csrfFetch } from './store/csrf';
 import { Provider } from 'react-redux';
-import configureStore from './store/store';
+import configureStore from './store/store.js';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
-
+import { Modal, ModalProvider } from './context/Modal.jsx';
 
 const store = configureStore();
 
@@ -15,18 +15,16 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-  window.sessionActions = sessionActions; 
-
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
