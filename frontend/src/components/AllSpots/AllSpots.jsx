23e6587c674b870d2c124604e1a1ebc5c668d176
bssam <bssammel@gmail.com>
2023-12-16 14:54:05 -0500

    Loading spot detail on the spots page, only working when navigating

--- frontend/src/components/AllSpots/AllSpots.jsx
+++ frontend/src/components/AllSpots/AllSpots.jsx
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 // import { NavLink } from "react-router-dom";
 import { getAllSpots } from "../../store/spots";
+import {getSpotDetails} from "../../store/spotDetails";
+
 
 const SpotList = () => {
     // console.log(state);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const spotList = useSelector((state) => Object.values(state.spot));
     console.log(7)
     console.log(spotList);
+
+ function goToSpot(spotId) {
+    dispatch(getSpotDetails(spotId));
+    navigate(`/spots/${spotId}`)
+
+ }
 
     useEffect(() => {
         console.log(8)
@@ -27,10 +37,10 @@ const SpotList = () => {
         <section className="spots-grid">
         {spotList[0]?.map((spot) =>(
            
-            <div key = {spot.id}>
+            <div key = {spot.id} onClick={() => goToSpot(spot.id)}>
         <p>This is {spot.name} for spot with {spot.id}. It is in {spot.city}, {spot.state}. With an average rating of {spot.avgRating} and a weekly price of {spot.price}, it is described as "{spot.description}". The image is below: </p>
         <img src={spot.previewImage} alt="" />
-        
+
         </div>
         ) 
         
--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -5,13 +5,13 @@ import {getSpotDetails} from "../../store/spotDetails";
 
 const SpotDetails = () => {
     const {spotId} = useParams();
-    const spotDetails = useSelector(
-        state => state.spot.entries.find(singleSpot => singleSpot.id === spotId )
-    )
+    // const spotDetails = useSelector(
+    //     state => state.spot.entries.find(singleSpot => singleSpot.id === spotId )
+    // )
     // const dispatch = useDispatch();
-    // const spotDetails = useSelector((state) => state.spotDetails);
-    // console.log("hey this is supposed to be the spotDetails obj")
-    // console.log(spotDetails);
+    const spotDetails = useSelector((state) => state.spotDetails.spotDetails);
+    console.log("hey this is supposed to be the spotDetails obj")
+    console.log(spotDetails.spotDetails);
 
     // useEffect(()=> {        
     //     console.log("this is the useEffect in spotdetailsjsx")
@@ -25,7 +25,6 @@ const SpotDetails = () => {
             <p className="location">{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</p>
             <h2>Hosted by: {spotDetails.Owner.firstName} {spotDetails.Owner.lastName}</h2>
             <p>{spotDetails.description}</p>
-            {}
         </>
     )
 }
