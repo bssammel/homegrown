dd07d110916b52c914d8837a4e2f5307e73fb7bc
bssam <bssammel@gmail.com>
2023-12-18 09:31:29 -0500

    Going back to this branch to try to figure out the useEffect issue.

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -1,32 +1,36 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {getSpotDetails} from "../../store/spotDetails";
+import { getSpotDetails } from "../../store/spotDetails";
 
 const SpotDetails = () => {
-    const {spotId} = useParams();
-    // const spotDetails = useSelector(
-    //     state => state.spot.entries.find(singleSpot => singleSpot.id === spotId )
-    // )
-    // const dispatch = useDispatch();
-    const spotDetails = useSelector((state) => state.spotDetails.spotDetails);
-    console.log("hey this is supposed to be the spotDetails obj")
-    console.log(spotDetails.spotDetails);
+  const { spotId } = useParams();
+  // const spotDetails = useSelector(
+  //     state => state.spot.entries.find(singleSpot => singleSpot.id === spotId )
+  // )
+  const dispatch = useDispatch();
+  const spotDetails = useSelector((state) => state.spotDetails.spotDetails);
+  console.log("hey this is supposed to be the spotDetails obj");
+  console.log(spotDetails.spotDetails);
 
-    // useEffect(()=> {        
-    //     console.log("this is the useEffect in spotdetailsjsx")
+  useEffect(() => {
+    console.log("this is the useEffect in spotdetailsjsx");
 
-    //     dispatch(getSpotDetails(parseInt(spotId)));
-    // }, [dispatch, spotId]);
+    dispatch(getSpotDetails(parseInt(spotId)));
+  }, [dispatch, spotId]);
 
-    return (
-        <>
-            <h1>{spotDetails.name}</h1>
-            <p className="location">{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</p>
-            <h2>Hosted by: {spotDetails.Owner.firstName} {spotDetails.Owner.lastName}</h2>
-            <p>{spotDetails.description}</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>{spotDetails.name}</h1>
+      <p className="location">
+        {spotDetails.city}, {spotDetails.state}, {spotDetails.country}
+      </p>
+      <h2>
+        Hosted by: {spotDetails.Owner.firstName} {spotDetails.Owner.lastName}
+      </h2>
+      <p>{spotDetails.description}</p>
+    </>
+  );
+};
 
 export default SpotDetails;
