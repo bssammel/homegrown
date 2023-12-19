d0fc71b6e1cb22c83f152232dae0c26104fd9f7a
bssam <bssammel@gmail.com>
2023-12-19 15:05:22 -0500

    added some styling to AllSpots

--- frontend/src/components/AllSpots/AllSpots.jsx
+++ frontend/src/components/AllSpots/AllSpots.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-// import { NavLink } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { getAllSpots } from "../../store/spots";
 
 const SpotList = () => {
@@ -27,16 +27,21 @@ const SpotList = () => {
       <h1>Grow Spots</h1>
       <section className="spots-grid">
         {spotList[0]?.map((spot) => (
-          // <div key = {spot.id} onClick={() => goToSpot(spot.id)}>
-          <div key={spot.id}>
-            <p>
-              This is {spot.name} for spot with {spot.id}. It is in {spot.city},{" "}
-              {spot.state}. With an average rating of {spot.avgRating} and a
-              weekly price of {spot.price}, it is described as "
-              {spot.description}". The image is below:{" "}
-            </p>
-            <img src={spot.previewImage} alt="" />
-          </div>
+          <NavLink key={spot.id} className="spotLink" to={`/spots/${spot.id}`}>
+            <div
+            //   key={spot.id}
+            //   className="single-spot"
+            // onClick={navigate(`/spots/${spot.id}`)}
+            >
+              <p>
+                This is {spot.name} for spot with {spot.id}. It is in{" "}
+                {spot.city}, {spot.state}. With an average rating of{" "}
+                {spot.avgRating} and a weekly price of {spot.price}, it is
+                described as "{spot.description}". The image is below:{" "}
+              </p>
+              <img src={spot.previewImage} alt="" />
+            </div>
+          </NavLink>
         ))}
       </section>
     </>
