84085c72f15e5bd3e837e4b07f8670c368e781c1
bssam <bssammel@gmail.com>
2024-01-02 13:43:11 -0500

    adjusting css for spotDetails

--- backend/db/seeders/20231003204404-demo-spot-images.js
+++ backend/db/seeders/20231003204404-demo-spot-images.js
@@ -1,46 +1,58 @@
-'use strict';
+"use strict";
 
-const {SpotImage} = require('../models');
+const { SpotImage } = require("../models");
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-  await SpotImage.bulkCreate([ {
-    spotId:1,
-    url:'https://gardeningsolutions.ifas.ufl.edu/images/plants/flowers/yellow_wildflowers_field.jpg',
-    preview:true
-   },
-   {
-    spotId:2,
-    url:'https://enjoyburlington.com/wp-content/uploads/sites/10/2015/06/Starr-farm-venuecrop.jpg',
-    preview:true
-   },
-   {
-    spotId:3,
-    url:'https://www.icloud.com/5220c404-6225-43fe-b9a5-41956640db08',
-    preview:true
-   },
-   {
-    spotId:3,
-    url:'https://www.icloud.com/158c08c8-4221-4fe8-91b3-747699ffa4e4',
-    preview:false
-   },
-  ],{validate: true});
+  async up(queryInterface, Sequelize) {
+    await SpotImage.bulkCreate(
+      [
+        {
+          spotId: 1,
+          url: "https://gardeningsolutions.ifas.ufl.edu/images/plants/flowers/yellow_wildflowers_field.jpg",
+          preview: true,
+        },
+        {
+          spotId: 2,
+          url: "https://enjoyburlington.com/wp-content/uploads/sites/10/2015/06/Starr-farm-venuecrop.jpg",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "https://i.pinimg.com/736x/02/6c/86/026c865915b553c221e173284022355e.jpg",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "https://sharonsantoni.com/wp-content/uploads/2020/06/my-french-country-home-june-garden-potager.jpg",
+          preview: false,
+        },
+      ],
+      { validate: true }
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     options.tableName = "SpotImages";
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      url: {[Op.in]: ["imageUrlForOakGroveTinyHome",
-        "imageUrlForLakesideCommunityGarden",
-        "imageUrlForSlopingBackyard1",
-        "imageUrlForSlopingBackyard2"]}
-    }, {})
-  }
+    return queryInterface.bulkDelete(
+      options,
+      {
+        url: {
+          [Op.in]: [
+            "https://gardeningsolutions.ifas.ufl.edu/images/plants/flowers/yellow_wildflowers_field.jpg",
+            "https://enjoyburlington.com/wp-content/uploads/sites/10/2015/06/Starr-farm-venuecrop.jpg",
+            "https://i.pinimg.com/736x/02/6c/86/026c865915b553c221e173284022355e.jpg",
+            "https://sharonsantoni.com/wp-content/uploads/2020/06/my-french-country-home-june-garden-potager.jpg",
+          ],
+        },
+      },
+      {}
+    );
+  },
 };
--- frontend/src/components/AllSpots/AllSpots.jsx
+++ frontend/src/components/AllSpots/AllSpots.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 // import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { getAllSpots } from "../../store/spots";
+import "./AllSpots.css";
 
 const SpotList = () => {
   // console.log(state);
@@ -25,24 +26,51 @@ const SpotList = () => {
   if (!Array.isArray(spotList[0])) {
     return <h1>Looking for spots in your area!</h1>;
   }
+  const hasReviews = function (spot) {
+    if (spot) {
+      if (spot.avgRating > 0) {
+        // console.log(`the current user is the author`);
+        return spot.avgRating;
+      } else {
+        // console.log(`The current user is not the author.`);
+        return "New";
+      }
+    } else return 0;
+  };
   return (
     <>
-      <h1>Grow Spots</h1>
+      <h1 id="all-spots-title">Grow Spots</h1>
       <section className="spots-grid">
         {spotList[0]?.map((spot) => (
-          <NavLink key={spot.id} className="spotLink" to={`/spots/${spot.id}`}>
+          <NavLink
+            key={spot.id}
+            className="single-spot"
+            to={`/spots/${spot.id}`}
+          >
             <div
-            //   key={spot.id}
-            //   className="single-spot"
-            // onClick={navigate(`/spots/${spot.id}`)}
+              className="spot-data"
+              //   key={spot.id}
+              //   className="single-spot"
+              // onClick={navigate(`/spots/${spot.id}`)}
             >
-              <p>
-                This is {spot.name} for spot with {spot.id}. It is in{" "}
-                {spot.city}, {spot.state}. With an average rating of{" "}
-                {spot.avgRating} and a weekly price of {spot.price}, it is
-                described as {`"${spot.description}"`}. The image is below:{" "}
-              </p>
-              <img src={spot.previewImage} alt="" />
+              <div className="image">
+                <img
+                  className="spot-image"
+                  src={spot.previewImage}
+                  alt=""
+                  // height={250}
+                  // width={250}
+                />
+              </div>
+
+              <div className="location-rating">
+                <p id="city-state">
+                  {spot.city}, {spot.state}
+                </p>
+                <p id="star-rating">★ {hasReviews(spot)}</p>
+              </div>
+
+              <p id="price">${spot.price} night</p>
             </div>
           </NavLink>
         ))}
