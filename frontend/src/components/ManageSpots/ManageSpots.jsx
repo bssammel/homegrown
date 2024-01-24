d10656fcb687e4642b005cf5289d603f9ca9d378
bssam <bssammel@gmail.com>
2024-01-24 18:40:40 -0500

    placing the toFixed back in

--- frontend/src/components/ManageSpots/ManageSpots.jsx
+++ frontend/src/components/ManageSpots/ManageSpots.jsx
@@ -69,9 +69,9 @@ const ManageSpots = () => {
       );
       if (spot.avgRating > 0 && typeof spot.avgRating === "number") {
         // console.log(`the current user is the author`);
-        // const formattedRating = spot.avgRating.toFixed(2);
-        // return formattedRating;
-        return spot.avgRating;
+        const formattedRating = spot.avgRating.toFixed(2);
+        return formattedRating;
+        // return spot.avgRating;
       } else {
         // console.log(`The current user is not the author.`);
         return "New";
