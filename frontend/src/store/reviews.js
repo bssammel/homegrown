7f3830df6217d248a3c300918435be7055eab1e4
bssam <bssammel@gmail.com>
2024-01-25 10:38:54 -0500

    throwing in a bunch of console.logs

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -6,6 +6,7 @@ const LOAD_SPOT_REVIEWS = "reviews/loadReviews";
 const DELETE_REVIEW = "reviews/deleteReviews";
 
 export const createReview = (newReview) => {
+  console.log("line 9 of reviews store: the create review action");
   return {
     type: CREATE_REVIEW,
     newReview,
@@ -13,6 +14,7 @@ export const createReview = (newReview) => {
 };
 
 export const loadSpotReviews = (reviews) => {
+  console.log("line 17 of reviews store: the load review action");
   return {
     type: LOAD_SPOT_REVIEWS,
     reviews,
@@ -20,6 +22,7 @@ export const loadSpotReviews = (reviews) => {
 };
 
 export const deleteSpotReview = (review) => {
+  console.log("line 25 of reviews store: the delete review action");
   console.log("delete review action");
   return {
     type: DELETE_REVIEW,
@@ -28,6 +31,7 @@ export const deleteSpotReview = (review) => {
 };
 
 export const createNewReview = (newReviewData, spotId) => async (dispatch) => {
+  console.log("line 34 of reviews store: the create review thunk");
   const res = await csrfFetch(`/api/spots/${spotId}/reviews`, {
     method: "POST",
     headers: {
@@ -45,7 +49,10 @@ export const createNewReview = (newReviewData, spotId) => async (dispatch) => {
 };
 
 export const getSpotReviews = (spotId) => async (dispatch) => {
+  console.log("line 52 of reviews store: the load review thunk");
   const res = await csrfFetch(`/api/spots/${spotId}/reviews`);
+  console.log("response at line 54");
+  console.log(res);
   if (res.ok) {
     const spotReviews = await res.json();
     dispatch(loadSpotReviews(spotReviews));
@@ -69,12 +76,16 @@ const reviewsReducer = (state = {}, action) => {
       return { ...state, [action.newReview.id]: action.newReview };
     }
     case LOAD_SPOT_REVIEWS: {
-      // console.log("Load spot reviews is running");
+      console.log("Load spot reviews reducer case is running");
       const newState = {};
+      console.log("line 81 of reviews store, the action.reviews:");
+      console.log(action.reviews);
       const reviewArr = Object.values(action.reviews);
       reviewArr.forEach((review) => {
         newState[review.id] = review;
       });
+      console.log("line 87, new state:");
+      console.log(newState);
       return newState;
     }
     default: {
