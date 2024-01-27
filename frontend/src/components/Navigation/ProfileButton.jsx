9ba749eeb5b36f67b5516f44aec1398a8f8106d7
bssam <bssammel@gmail.com>
2024-01-27 17:04:56 -0500

    placing remove event listener again

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -33,11 +33,10 @@ function ProfileButton({ user }) {
         console.log("close menu setting false");
         setShowMenu(false);
       }
+      return () => document.removeEventListener("click");
     };
 
     document.addEventListener("click", closeMenu());
-
-    // return () => document.removeEventListener("click");
   }, []);
 
   // const closeMenu = () => setShowMenu(false);
