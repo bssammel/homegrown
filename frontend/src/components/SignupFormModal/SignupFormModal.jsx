7b38562e2093b65ea9330b66f9c7ba6410e85bb2
bssam <bssammel@gmail.com>
2024-01-27 22:24:52 -0500

    css updated to match wireframe

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -52,87 +52,113 @@ function SignupFormModal() {
     <>
       <form onSubmit={handleSubmit}>
         <h1>Sign Up</h1>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && (
-          <p className="error-message">{"The provided email is invalid"}</p>
-        )}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p className="error-message">{errors.username}</p>}
-        {username.length < 4 && (
-          <p className="error-message">
-            {"Your username must be at least 4 characters."}
-          </p>
-        )}
-        <label>
-          First Name
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.firstName && (
-          <p className="error-message">{errors.firstName}</p>
-        )}
-        <label>
-          Last Name
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.lastName && <p className="error-message">{errors.lastName}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p className="error-message">{errors.password}</p>}
-        {password.length < 4 && (
-          <p className="error-message">
-            {"Your password must be at least 6 characters."}
-          </p>
-        )}
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && (
-          <p className="error-message">{errors.confirmPassword}</p>
-        )}
-        {confirmPassword !== password && (
-          <p className="error-message">
-            It looks like your passwords do not match, please check again.
-          </p>
-        )}
+        <div id="fields">
+          <section id="first-name">
+            <div className="indiv-field">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                name="firstName"
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+            {errors.firstName && (
+              <p className="error-message">{errors.firstName}</p>
+            )}
+          </section>
+          <section id="last-name">
+            <div className="indiv-field">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                name="lastName"
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            {errors.lastName && (
+              <p className="error-message">{errors.lastName}</p>
+            )}
+          </section>
+          <section id="email">
+            <div className="indiv-field">
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            {errors.email && (
+              <p className="error-message">{"The provided email is invalid"}</p>
+            )}
+          </section>
+          <section id="username">
+            <div className="indiv-field">
+              <label htmlFor="username">Username</label>
+              <input
+                name="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            {errors.username && (
+              <p className="error-message">{errors.username}</p>
+            )}
+            {username.length < 4 && (
+              <p className="error-message">
+                {"Your username must be at least 4 characters."}
+              </p>
+            )}
+          </section>
+          <section id="password">
+            <div className="indiv-field">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            {errors.password && (
+              <p className="error-message">{errors.password}</p>
+            )}
+            {password.length < 4 && (
+              <p className="error-message">
+                {"Your password must be at least 6 characters."}
+              </p>
+            )}
+          </section>
+          <section id="confirm-password">
+            <div className="indiv-field">
+              <label>Confirm Password</label>
+              <br />
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+            {errors.confirmPassword && (
+              <p className="error-message">{errors.confirmPassword}</p>
+            )}
+            {confirmPassword !== password && (
+              <p className="error-message">
+                It looks like your passwords do not match, please check again.
+              </p>
+            )}
+          </section>
+        </div>
         <button
           type="submit"
           disabled={
